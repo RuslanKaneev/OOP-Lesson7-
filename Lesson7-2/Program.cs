8a1539f3f3252d5dd64752871d58804b63acb11d
Руслан <karuslan@inbox.ru>
2022-05-05 18:55:31 +0300

    версия 1.0

---
+++ Lesson7-2/Program.cs
@@ -0,0 +1,17 @@
+﻿
+    namespace Lesson7_2
+    {
+        public class Program
+        {
+            static void Main(string[] args)
+            {
+
+            }
+        }
+        public enum VisibilityStatus
+        {
+            visible,
+            invisible
+
+        }
+    }
---
+++ Lesson7-2/Rectangle.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson7_2
+{
+    internal class Rectangle : Point
+
+    {
+        public int Leight { get; set; }
+        public int Height { get; set; }
+
+
+        public Rectangle(int x, int y, int leight, int height) : base(x, y)
+        {
+            var x1y1 = new Point(X, Y);
+            var x1y2 = new Point(X, Y + height);
+            var x2y1 = new Point(X + leight, Y);
+            var x2y2 = new Point(X + leight, Y + height);
+
+        }
+
+        public double SquareArea(int leight, int height)
+        {
+            return leight * height;
+        }
+        public override Point MoveFigure(int x, int y)
+        {
+            return base.MoveFigure(x, y);
+        }
+        public override string ToString()
+        {
+            return base.ToString() + "Длина: " + Leight + " Ширина: " + Height + " Площадь: " + SquareArea(Leight, Height);
+        }
+
+    }
+}
